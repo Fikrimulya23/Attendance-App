810fe42898cc2c1f5966807ab1367a98e600d2a2
Fikri Mulya Permana <fikrimulyapermana00@gmail.com>
2024-03-08 11:21:56 +0700

    get my location

--- lib/main_map.dart
+++ lib/main_map.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:attendance_app/location_service.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MainMap extends StatefulWidget {
@@ -20,11 +21,14 @@ class MainMapState extends State<MainMap> {
     zoom: 14.4746,
   );
 
-  static const CameraPosition _kLake = CameraPosition(
+  /* static const CameraPosition _kLake = CameraPosition(
       bearing: 192.8334901395799,
       target: LatLng(-6.2779792, 106.8029257),
       tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
+      zoom: 19.151926040649414); */
+
+  Color mainColor = Color(0XFF59D5E0);
+  Color whiteColor = Color(0XFFF6F5F5);
 
   @override
   Widget build(BuildContext context) {
@@ -33,25 +37,81 @@ class MainMapState extends State<MainMap> {
         mapType: MapType.normal,
         initialCameraPosition: _kGooglePlex,
         compassEnabled: true,
-        indoorViewEnabled: true,
         myLocationEnabled: true,
         myLocationButtonEnabled: true,
+        zoomControlsEnabled: false,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
       ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
+      floatingActionButton: Container(
+        // color: Colors.amber.withOpacity(0.3),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: Container(
+                width: 40,
+                height: 40,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(12),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 2,
+                        color: Colors.grey.withOpacity(0.2),
+                        offset: Offset(0, 0),
+                        spreadRadius: 1,
+                      )
+                    ]),
+                child: Icon(Icons.add),
+              ),
+            ),
+            IconButton(
+              onPressed: _goToMyLocation,
+              icon: Container(
+                width: 60,
+                height: 60,
+                decoration: BoxDecoration(
+                  color: mainColor,
+                  borderRadius: BorderRadius.circular(12),
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 2,
+                      color: Colors.grey.withOpacity(0.2),
+                      offset: Offset(0, 0),
+                      spreadRadius: 1,
+                    )
+                  ],
+                ),
+                child: const Icon(
+                  Icons.my_location_rounded,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      /* floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToMyLocation,
         label: const Text('To the lake!'),
         icon: const Icon(Icons.directions_boat),
-      ),
+      ), */
     );
   }
 
-  Future<void> _goToTheLake() async {
-    var myLocation = getMyLocation();
-    print(myLocation);
-    // final GoogleMapController controller = await _controller.future;
-    // await controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  Future<void> _goToMyLocation() async {
+    Position myLocation = await getMyLocation();
+    CameraPosition newCameraPosition = CameraPosition(
+      target: LatLng(myLocation.latitude, myLocation.longitude),
+      zoom: 19,
+    );
+    final GoogleMapController controller = await _controller.future;
+    await controller.animateCamera(
+      CameraUpdate.newCameraPosition(newCameraPosition),
+    );
   }
 }
